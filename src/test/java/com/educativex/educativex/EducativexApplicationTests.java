e05b9f56d1ff9c2ecd381642caaed14de6d292c8
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2026-01-07 16:31:11 -0300

    Primeiro commit

---
+++ src/test/java/com/educativex/educativex/EducativexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educativex.educativex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducativexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
