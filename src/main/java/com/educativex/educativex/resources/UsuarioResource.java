8a9926975c4fa24842c836494050614ee2ff08fe
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2026-02-10 17:57:23 -0300

    adicionando os métodos ao UsuarioResource 1

--- src/main/java/com/educativex/educativex/resources/UsuarioResource.java
+++ src/main/java/com/educativex/educativex/resources/UsuarioResource.java
@@ -1,12 +1,17 @@
 package com.educativex.educativex.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educativex.educativex.entities.Usuario;
 import com.educativex.educativex.servicies.UsuarioService;
@@ -23,6 +28,19 @@ public class UsuarioResource {
 	public ResponseEntity<List<Usuario>> findAll(){
 		List<Usuario> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable String id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Void> insert(@RequestBody Usuario usuario){
+		Usuario obj = service.insert(usuario);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
